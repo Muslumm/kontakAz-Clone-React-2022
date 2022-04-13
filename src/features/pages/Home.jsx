3a87085a601aba041bbe5463aa7d8f47fba76bfa
rasim hesenov <rasimh989h@gmail.com>
2022-04-13 19:25:08 +0400

    updated slider component

--- src/features/pages/Home.jsx
+++ src/features/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Info from '../components/info_section/Info'
 import Categories from '../components/categories/Categories'
-import Carusel from '../components/carusel/Carusel'
+import Slider from '../components/slider/Slider'
 import './home.css'
+
 
 
 function Home() {
@@ -11,7 +12,7 @@ function Home() {
        <Info/>
        <div className="content">
            <Categories/>
-           <Carusel/>
+           <Slider/>
        </div>
     </div>
   )
