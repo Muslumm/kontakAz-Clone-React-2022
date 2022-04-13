3a9f72734dc700685f067e0561b366e9dbc254b0
rasim hesenov <rasimh989h@gmail.com>
2022-04-13 15:00:32 +0400

    updated submenu of categories

--- src/features/components/categories/Categories.jsx
+++ src/features/components/categories/Categories.jsx
@@ -8,11 +8,27 @@ function Categories() {
 
   const categories = useSelector(categoriesSelectors.selectAll)
 
+  // first categories
   const renderedCategories = categories.map(link => {
     return <div className="category" key={link.id}>
-      <NavLink to={link.link} className='category_link'>{link.name}</NavLink>
+      <NavLink to={link.link} className='category_link'>{link.name}
+      <ul  className='sub_menu'>
+               {
+                   link.hasSubMenu && (
+                    link.subMenu.map(menu=>{
+                        return  <li className='subMenu_List'>
+                        <NavLink to={menu.link} className="subMenu_Link">{menu.name}</NavLink>
+                   </li>
+                    })
+                   )
+               }
+           </ul>
+      </NavLink>
     </div>
   })
+
+  // subMenus 
+
 
   return (
     <div className='categories_section'>
--- src/features/components/categories/categoriesData.js
+++ src/features/components/categories/categoriesData.js
@@ -1,16 +1,109 @@
 export const categoriesData = {
     ids: ["id1","id2","id3","id4","id5","id6","id7","id8","id9","id10","id11"],
-    entities:{
-        id1: {id:"id1",name:"Telefonlar ve Plansetler",link:"/",hasSubMenu:true,},
-        id2: {id:"id2",name:"Saatlar ve qolbaqlar",link:"/",hasSubMenu:true,},
-        id3: {id:"id3",name:"Tv,audio video",link:"/",hasSubMenu:true,},
-        id4: {id:"id4",name:"Komputer ve aksesuarlar",link:"/",hasSubMenu:true,},
-        id5: {id:"id5",name:"Meiset Texnikasi",link:"/",hasSubMenu:true,},
-        id6: {id:"id6",name:"Parfumeriya ve Kosmetika",link:"/",hasSubMenu:true,},
-        id7: {id:"id7",name:"Foto ve video",link:"/",hasSubMenu:true,},
-        id8: {id:"id8",name:"Mebel ve Ev Tekstili",link:"/",hasSubMenu:true,},
-        id9: {id:"id9",name:"Hobbi ve Eylence",link:"/",hasSubMenu:true,},
-        id10: {id:"id10",name:"Mebel ve Ev Tekstili",link:"/",hasSubMenu:true,},
-        id11: {id:"id11",name:"Ev ve Bag",link:"/",hasSubMenu:true,},
+    entities:{ 
+        id1: {id:"id1",name:"Telefonlar ve Plansetler",link:"/",hasSubMenu:true,
+             subMenu:[
+                 {id:"sub_1",name:"Smartfonlar",link:"/",hasSubMenu:true},
+                 {id:"sub_2",name:"Plansetler",link:"/",hasSubMenu:true},
+                 {id:"sub_3",name:"Qulaqliqlar",link:"/",hasSubMenu:true},
+                 {id:"sub_4",name:"Duymeli Telefonlar",link:"/",hasSubMenu:true},
+                 {id:"sub_5",name:"Ev ve Ofis Telefonlari",link:"/",hasSubMenu:true},
+                 {id:"sub_6",name:"Elektron Kitab",link:"/",hasSubMenu:true},
+             ]},
+
+        id2: {id:"id2",name:"Saatlar ve qolbaqlar",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_7",name:"Smart saatlarr",link:"/",hasSubMenu:true},
+            {id:"sub_8",name:"Smart qolbaqlar",link:"/",hasSubMenu:true},
+            {id:"sub_9",name:"Saatlar",link:"/",hasSubMenu:true},
+            {id:"sub_10",name:"Usaq Saatlari",link:"/",hasSubMenu:true},
+            {id:"sub_11",name:"Aksesuarlar",link:"/",hasSubMenu:true},
+        ]},
+
+        id3: {id:"id3",name:"Tv,audio video",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_10",name:"Telvizorlar",link:"/",hasSubMenu:true},
+            {id:"sub_11",name:"Proyektor ve Ekran",link:"/",hasSubMenu:true},
+            {id:"sub_12",name:"Auido,Video",link:"/",hasSubMenu:true},
+            {id:"sub_13",name:"Televizor Altligi",link:"/",hasSubMenu:true},
+            {id:"sub_14",name:"Aksesuarlar",link:"/",hasSubMenu:true},
+        ]},
+
+        id4: {id:"id4",name:"Komputer ve aksesuarlar",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_15",name:"Apple",link:"/",hasSubMenu:true},
+            {id:"sub_16",name:"Notebooklar",link:"/",hasSubMenu:true},
+            {id:"sub_17",name:"Manitor",link:"/",hasSubMenu:true},
+            {id:"sub_18",name:"Sistem Bloklari",link:"/",hasSubMenu:true},
+            {id:"sub_19",name:"Plansetler",link:"/",hasSubMenu:true},
+            {id:"sub_20",name:"Printerler",link:"/",hasSubMenu:true},
+            {id:"sub_21",name:"MonoBlock",link:"/",hasSubMenu:true},
+        ]},
+
+        id5: {id:"id5",name:"Meiset Texnikasi",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_22",name:"Boyuk Meiset Texnikasi",link:"/",hasSubMenu:true},
+            {id:"sub_23",name:"Iqlim Texnikasi",link:"/",hasSubMenu:true},
+            {id:"sub_24",name:"Ev ucun kicik meiset texnikasi",link:"/",hasSubMenu:true},
+            {id:"sub_25",name:"Pesekar temizlik avadanligi",link:"/",hasSubMenu:true},
+            {id:"sub_26",name:"Metbex ucun kicik meiset texnikasi",link:"/",hasSubMenu:true},
+            {id:"sub_27",name:"Metbex ucun qurasdirilmis texnika",link:"/",hasSubMenu:true},         
+        ]},
+
+        id6: {id:"id6",name:"Parfumeriya ve Kosmetika",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_28",name:"Etir",link:"/",hasSubMenu:true},
+            {id:"sub_29",name:"Parfumlar",link:"/",hasSubMenu:true},
+            {id:"sub_30",name:"Pamadalar",link:"/",hasSubMenu:true},
+            {id:"sub_31",name:"rujlar",link:"/",hasSubMenu:true},
+        ]},
+
+        id7: {id:"id7",name:"Foto ve video",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_32",name:"Ani Cap Fotoaparatlar",link:"/",hasSubMenu:true},
+            {id:"sub_33",name:"Pesekar fotoaparatlar",link:"/",hasSubMenu:true},
+            {id:"sub_34",name:"Foto Aksesuar",link:"/",hasSubMenu:true},
+            {id:"sub_35",name:"Tehlukesizlik Kameralari",link:"/",hasSubMenu:true},
+        ]},
+
+        id8: {id:"id8",name:"Mebel ve Ev Tekstili",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_36",name:"Ev Mebeli",link:"/",hasSubMenu:true},
+            {id:"sub_37",name:"Ev Tekstili",link:"/",hasSubMenu:true},
+            {id:"sub_38",name:"Matras",link:"/",hasSubMenu:true},
+            {id:"sub_39",name:"Divar Kagizlari",link:"/",hasSubMenu:true},
+            {id:"sub_40",name:"Ofis Mebeli",link:"/",hasSubMenu:true},
+            {id:"sub_41",name:"Xalcalar",link:"/",hasSubMenu:true},
+        ]},
+
+        id9: {id:"id9",name:"Hobbi ve Eylence",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_42",name:"Musiqi aletleri ve avadanliqlari",link:"/",hasSubMenu:true},
+            {id:"sub_43",name:"Plansetler",link:"/",hasSubMenu:true},
+            {id:"sub_44",name:"Idman aletleri",link:"/",hasSubMenu:true},
+            {id:"sub_45",name:"Neqliyyat",link:"/",hasSubMenu:true},
+            {id:"sub_46",name:"Plastation",link:"/",hasSubMenu:true},
+            {id:"sub_47",name:"Oyun Konsollari",link:"/",hasSubMenu:true},
+        ]},
+        id10: {id:"id10",name:"Ev ve Bag",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_48",name:"Nerdivanlar",link:"/",hasSubMenu:true},
+            {id:"sub_49",name:"Bag Texnikasi",link:"/",hasSubMenu:true},
+            {id:"sub_50",name:"Lampalar",link:"/",hasSubMenu:true},
+            {id:"sub_51",name:"Agilli Ev",link:"/",hasSubMenu:true},
+            {id:"sub_52",name:"Elektrik aletler",link:"/",hasSubMenu:true},
+            {id:"sub_53",name:"Bag aletleri",link:"/",hasSubMenu:true},
+        ]},
+
+        id11: {id:"id11",name:"Avtomobil Mehsullari",link:"/",hasSubMenu:true,
+        subMenu:[
+            {id:"sub_54",name:"Avtomobiller ucun adabterler",link:"/",hasSubMenu:true},
+            {id:"sub_55",name:"Video Qeydiyyatci",link:"/",hasSubMenu:true},
+            {id:"sub_56",name:"Tekerler",link:"/",hasSubMenu:true},
+            {id:"sub_57",name:"Akkumlyatorlar",link:"/",hasSubMenu:true},
+            {id:"sub_58",name:"Yaglar",link:"/",hasSubMenu:true},
+        ]},
+
     }
+
 }
